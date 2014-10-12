37949f4f5175f4a6a6f403df9d6363f6e97532f0
Danya Kim <itsdanya@gmail.com>
2014-10-12 14:48:40 +0400

    Implement counting sort

--- Project4/Project4/main.cpp
+++ Project4/Project4/main.cpp
@@ -7,15 +7,139 @@
 //
 
 #include <iostream>
+#include <fstream>
 #include "sortings.h"
 
+using namespace std;
+
+void read_arrays();
+void read_input(int &param, string message);
+void copy_array(int array1[], int array2[], int n);
+void sort_array(int array[], int n);
+
 int main() {
-    int *a;
-    a = new int[10];
+    // User's choice
+    int choice;
+    cout << "1. Generate an array\n2. Read from file" << endl;
+    cin >> choice;
 
-    for (int i = 0; i < 10; i++) {
-        a[i] = 0;
+    switch (choice) {
+    case 1:
+        // Generate an array
+        int array_size;
+        read_input(array_size, "Enter array size: ");
+
+        int min;
+        read_input(min, "Enter minimum value: ");
+
+        int max;
+        read_input(max, "Enter maximum value: ");
+
+        cout << endl;
+
+        int *array;
+        array = new int[array_size];
+
+        array_generation(array, array_size, min, max);
+        sort_array(array, array_size);
+
+        delete[] array;
+        break;
+    case 2:
+        // Read arrays from file
+        read_arrays();
+        break;
+
+    default:
+        cout << "Error" << endl;
+        break;
     }
+}
 
-    counting_sort(a, 10);
+// Read integer input from the console
+void read_input(int &param, string message) {
+    while (true) {
+        cout << message;
+        if (cin >> param) {
+            break;
+        }
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+}
+
+// Read and sort arrays from file
+void read_arrays() {
+    string line;
+    ifstream input_file;
+    input_file.open("test_proekt_3.txt");
+
+    if (input_file.is_open()) {
+        // Get arrays count
+        int n;
+        input_file >> n;
+        cout << "Arrays: " << n << "\n" << endl;
+
+        // Read arrays
+        for (int i = 0; i < n; i++) {
+            // Get array size
+            int array_size;
+            input_file >> array_size;
+            cout << "Array size: " << array_size << endl;
+
+            int *array;
+            array = new int[array_size];
+
+            // Read array elements
+            for (int j = 0; j < array_size; j++) {
+                int element;
+                input_file >> element;
+                array[j] = element;
+            }
+
+            // Sort array
+            sort_array(array, array_size);
+
+            if (i != n - 1) {
+                cout << endl;
+            }
+            delete[] array;
+        }
+    } else {
+        cout << "Unable to open" << endl;
+    }
+}
+
+// Copy array1 to array2
+void copy_array(int array1[], int array2[], int n) {
+    for (int i = 0; i < n; i++) {
+        array2[i] = array1[i];
+    }
+}
+
+// Sort the array and pretty print the result
+void sort_array(int array[], int n) {
+    // Sort copied array, not the original one
+    int *array_copy;
+    array_copy = new int[n];
+
+    // Print array
+    cout << "Array before: " << endl;
+    results(array, n);
+
+    // Counting sort
+    cout << "Counting sort: " << endl;
+    copy_array(array, array_copy, n);
+    counting_sort(array_copy, n);
+    results(array_copy, n);
+
+    //
+    //    // Binary sort
+    //    cout << "Binary sort: " << endl;
+    //    copy_array(array, array_copy, n);
+    //    //    binary_sort(array_copy, n);
+    //    results(array_copy, n);
+
+    // Clean up
+    delete[] array_copy;
 }
--- Project4/Project4/sortings.cpp
+++ Project4/Project4/sortings.cpp
@@ -8,26 +8,46 @@
 
 #include "sortings.h"
 
+bool verify_array(int a[], int n);
+int max_value(int a[], int n);
+void copy_arrays(int array1[], int array2[], int n);
+
 void counting_sort(int a[], int n) {
+    // Throw exception unless valid
+    if (!verify_array(a, n)) {
+        throw invalid_argument("Array elements should be less than zero.");
+    }
+
+    int max = max_value(a, n);
+
+    int *b;
+    b = new int[n];
+
     int *c;
-    c = new int[n];
+    c = new int[max + 1];
 
     // Fill array with 0s
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i <= max; i++) {
         c[i] = 0;
     }
 
-    for (int i = 0; i < n - 1; i++) {
-        c[a[i]] = c[a[i]] + 1;
+    for (int i = 0; i < n; i++) {
+        c[a[i]]++;
     }
 
-    for (int j = 0; j < n; j++) {
-        c[j] = c[j] + c[j - 1];
+    for (int j = 0; j <= max; j++) {
+        c[j] += c[j - 1];
     }
 
     for (int i = n - 1; i >= 0; i--) {
-        cout << "Hello" << endl;
+        b[c[a[i]] - 1] = a[i];
+        c[a[i]] -= 1;
     }
+
+    // Clean up
+    copy_arrays(b, a, n);
+    delete[] b;
+    delete[] c;
 }
 
 void radix_sort10(int a[], int n) {
@@ -41,15 +61,58 @@ void radix_sort256(int a[], int n) {
 }
 
 void array_generation(int result[], int n, int min, int max) {
-    // TODO: you need to provide your array generation
+    for (int i = 0; i < n; i++) {
+        result[i] = min + rand() % (max - min + 1);
+    }
 }
 
 void results(int a[], int n) {
-    // TODO: you need to output array, use standart c++ output
+    cout << "[";
+    for (int i = 0; i < n; i++) {
+        cout << a[i];
+        if (i != n - 1) {
+            cout << ", ";
+        }
+    }
+    cout << "]" << endl;
 }
 
 bool is_sort(int a[], int n) {
-    // TODO: you need to check out your array is sorted or not
-    // return false or true;
+    bool flag = true;
+    for (int i = 0; i < n - 1; i++) {
+        if (a[i + 1] > a[i]) {
+            flag = false;
+        }
+    }
+    return flag;
+}
+
+// Auxiliary
+
+// Elements should be > 0
+bool verify_array(int a[], int n) {
+    for (int i = 0; i < n; i++) {
+        if (a[i] < 0) {
+            return false;
+        }
+    }
     return true;
+}
+
+// Max array value, assuming elements are positive
+int max_value(int a[], int n) {
+    int max = 0;
+    for (int i = 0; i < n; i++) {
+        if (a[i] > max) {
+            max = a[i];
+        }
+    }
+    return max;
+}
+
+// Copy array1 to array2
+void copy_arrays(int array1[], int array2[], int n) {
+    for (int i = 0; i < n; i++) {
+        array2[i] = array1[i];
+    }
 }
